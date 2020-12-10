0af1b245c61137db2955c68255e22edc914bdbb7
tom_glb <yuan_java@163.com>
2020-12-11 00:26:39 +0800

    feat: 新建表

---
+++ cloud-provider-payment8001/src/main/java/dao/PaymentDao.java
@@ -0,0 +1,17 @@
+package dao;
+
+import entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * @author yuanhaichao
+ * @date 2020/12/11 - 0:13
+ */
+@Mapper
+public interface PaymentDao {
+
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
---
+++ cloud-provider-payment8001/src/main/java/entities/Payment.java
@@ -0,0 +1,20 @@
+package entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author yuanhaichao
+ * @date 2020/12/11 - 0:09
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    private String serial;
+}
+
