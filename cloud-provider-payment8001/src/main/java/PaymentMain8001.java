20b25b1f2caf50396a450213cb4e0d07b7bb0e4a
tom_glb <yuan_java@163.com>
2020-12-11 00:00:09 +0800

    feat: 新建支付启动类

---
+++ cloud-provider-payment8001/src/main/java/PaymentMain8001.java
@@ -0,0 +1,17 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuanhaichao
+ * @date 2020/12/10 - 23:55
+ */
+
+@SpringBootApplication
+public class PaymentMain8001 {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PaymentMain8001.class, args);
+
+    }
+}
